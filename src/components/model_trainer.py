9034d1f382ab0d1eee38877eb09ace65b5e0970a
Yaswanth3 <katari.yaswanth@gmail.com>
2025-02-04 01:16:51 +0530

    Model Trainer

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -13,6 +13,7 @@ from sklearn.metrics import r2_score
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.tree import DecisionTreeRegressor
 from xgboost import XGBRegressor
+from sklearn.model_selection import GridSearchCV
 
 from src.exception import CustomException
 from src.logger import logging
@@ -46,8 +47,38 @@ class ModelTrainer:
                 "CatBoos Regressor": CatBoostRegressor(verbose=False),
                 "AdaBoost Regressor": AdaBoostRegressor()
             }
+            params={
+                "Random Forest":{
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Decision Tree":{
+                    'criterion':['squared_error', 'friedman_mse','absolute_error','poisson']
+                },
+                "Gradient Boosting":{
+                    'learning_rate':[0.1,0.01,0.05,0.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Linear Regression":{},
+                "K-Neighbors Regressor":{
+                    'n_neighbors':[5,7,9,11],
+                },
+                "XGBoost Regressor":{
+                    'learning_rate':[0.1,0.01,0.05,0.001],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "CatBoos Regressor":{
+                    'depth': [6,8,10],
+                    'learning_rate': [0.01,0.05,0.1],
+                    'iterations': [30,50,100]
+                },
+                "AdaBoost Regressor":{
+                    'learning_rate': [0.1,0.01,0.5,0.001],
+                    'n_estimators': [8,16,32,64,128,256]
+                }
+            }
 
-            model_report:dict=evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,models=models)
+            model_report:dict=evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,models=models,params=params)
 
             ## To get best model score from dict
             best_model_score = max(sorted(model_report.values()))
